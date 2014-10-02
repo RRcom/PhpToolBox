2a3366b0f3c8f40d75bf9a0b7f4d4d0d8b1513d0
Resty <resty_rizal@live.com>
2014-10-02 13:27:52 +0800

    set default config

--- libraries/RRcom/PhpToolBox/Config.php
+++ libraries/RRcom/PhpToolBox/Config.php
@@ -5,6 +5,7 @@ class Config {
 
     const DEFAULT_CONFIG_DIR = '../../../config/';
     const DEFAULT_CONFIG_FILE = 'config.local.php';
+	const SAMPLE_CONFIG_FILE = 'sample.config.local.php';
 
     public $config;
     
@@ -35,9 +36,7 @@ class Config {
     static function getConfigFromFile($configFile = '') {
         if(is_file($configFile)) return (array) include $configFile;
         if(!is_file(__DIR__.'/'.self::DEFAULT_CONFIG_DIR.self::DEFAULT_CONFIG_FILE)) {
-            $cfgFile = fopen(__DIR__.'/'.self::DEFAULT_CONFIG_DIR.self::DEFAULT_CONFIG_FILE, 'w');
-            fwrite($cfgFile, file_get_contents(__DIR__.'/'.self::DEFAULT_CONFIG_DIR.'sample.config.php'));
-            fclose($cfgFile);
+            return (array) include __DIR__.'/'.self::DEFAULT_CONFIG_DIR.self::SAMPLE_CONFIG_FILE;
         }
         return (array) include __DIR__.'/'.self::DEFAULT_CONFIG_DIR.self::DEFAULT_CONFIG_FILE;
     }
